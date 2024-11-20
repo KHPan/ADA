e1dfc2b28faca8d4ab84526544a585f211057b73
hank940115 <146960977+hank940115@users.noreply.github.com>
2024-11-20 18:40:20 +0800

    Update ada.py

--- ada.py
+++ ada.py
@@ -20,7 +20,7 @@ for count in range(T):
 	G.remove_nodes_from(nx.articulation_points(G))
 	P_ans = N * (N-1) // 2
 	for g in nx.connected_components(G):
-		P_ans -= g.number_of_nodes() * (g.number_of_nodes()-1) // 2
+		P_ans -= len(g) * (len(g)-1) // 2
 	if C_ans == P_ans:
 		print(f"Correct {count+1}/{T}")
 	else:
